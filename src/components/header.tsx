203b7cb30b3cd170ef8e0100b2465b577f2d05f7
Ryoya Chiba <ryokan.jap.83.c@gmail.com>
2024-08-02 23:41:24 +0900

    feat: header design

---
+++ src/components/header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="navbar bg-blue-300">
+      <div className="navbar-start">
+        <a href="/">
+          <div className="site-top flex">
+            {/* ロゴおよびサイト名は変更予定 */}
+            <div className="logo px-2">LOGO</div>
+            <div className="site-name">bookshelf</div>
+          </div>
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul tabIndex={0} className="menu menu-horizontal px-1">
+          <li className="nav-item">
+            <a>Item1</a>
+          </li>
+          <li className="nav-item">
+            <a>Item2</a>
+          </li>
+          <li className="nav-item">
+            <a>Item3</a>
+          </li>
+          <li className="nav-item">
+            <a>Item4</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="avatar">
+          <div className="w-16 rounded-full">
+            {/* 画像は各ユーザーの画像を表示する */}
+            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
