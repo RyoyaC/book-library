93399112b1ad0f3332d6796ad8d1dbf50857b835
Ryoya Chiba <ryokan.jap.83.c@gmail.com>
2024-08-10 23:45:43 +0900

    feat: created login page

---
+++ src/app/account/login/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="py-20 flex justify-center">
+      <div className="card card-bordered bg-white w-full max-w-md">
+        <div className="card-title flex justify-center mt-4">
+          <p className="font-bold">Login</p>
+        </div>
+        <div className="card-body flex flex-col items-center">
+          <div className="form-group mb-2 w-96">
+            <label className="text-gray-700 text-sm font-bold">Username</label>
+            <input
+              className="border rounded w-full py-2 px-3 text-gray-700 shadow focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div className="form-group mb-4 w-96">
+            <label className="text-gray-700 text-sm font-bold">Password</label>
+            <input
+              className="border rounded w-full py-2 px-3 text-gray-700 shadow focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="login-button flex justify-center">
+            <button className="btn btn-active btn-neutral rounded-lg w-24 bg-blue-500 hover:bg-blue-700 border-transparent text-white font-bold rounded focus:outline-none focus:shadow-outline">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
